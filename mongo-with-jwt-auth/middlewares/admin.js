fae4ffb2190d831c5b42b2acecb339d08a5fc03a
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-07-29 21:19:40 +0530

    Added Admin endpoints

--- mongo-with-jwt-auth/middlewares/admin.js
+++ mongo-with-jwt-auth/middlewares/admin.js
@@ -1,18 +1,29 @@
 const zod = require('zod');
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret'
-function adminMiddleware() {
-    const username = req.body.username;
-    const password = req.body.password;
-    const usernameSchema = zod.string().email();
-    const passwordSchema = zod.string().min(4);
-    const usernameResponse = usernameSchema.safeParse(username);
-    const passwordResponse = passwordSchema.safeParse(password);
-    if (usernameResponse.success && passwordResponse.success) {
-        const token = jwt.sign({
-            username
-        }, jwtPassword);
-        req.token = token
+function adminMiddleware(req, res, next) {
+    /*     const username = req.body.username;
+        const password = req.body.password;
+        const usernameSchema = zod.string().email();
+        const passwordSchema = zod.string().min(4);
+        const usernameResponse = usernameSchema.safeParse(username);
+        const passwordResponse = passwordSchema.safeParse(password);
+        if (usernameResponse.success && passwordResponse.success) {
+            const token = jwt.sign({
+                username
+            }, jwtPassword);
+            req.token = token
+        } */
+    try {
+        const token = req.headers.token;
+        const response = jwt.verify(token, jwtPassword);
+        if (response) {
+            next();
+        }
+    } catch (error) {
+        res.json(500).json({
+            msg: 'Invalid token'
+        })
     }
 
 }
--- mongo-with-jwt-auth/routes/admin.js
+++ mongo-with-jwt-auth/routes/admin.js
@@ -1,37 +1,75 @@
 const { Router } = require("express");
 const adminMiddleware = require('../middlewares/admin');
-const { Admin } = require("../db");
+const { Admin, Course } = require("../db");
 const router = Router();
+const jwt = require('jsonwebtoken');
+const jwtPassword = 'secret'
 
 // Admin Routes
 router.post('/signup', async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
-  
+
     Admin.create({
         username,
         password
     })
 
     res.json({
-        msg : 'Admin created successfully !!!'
+        msg: 'Admin created successfully !!!'
     })
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
-    
+    const response = await Admin.findOne({ username });
+    let token = null;
+    if (response) {
+        token = jwt.sign({
+            username
+        }, jwtPassword);
+    } else {
+        res.status(404).json({
+            msg: 'User not found'
+        })
+    }
+    console.log(response);
+
+
+    res.json({
+        token: token
+    })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+
+    const newCourse = await Course.create({
+        title,
+        description,
+        price,
+        imageLink,
+    })
+
+    res.json(
+        { message: 'Course created successfully', courseId: newCourse._id }
+    )
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+  const response = courses = await Course.find({});
+console.log(response);
+  res.json({
+    courses : response
+  })
 });
 
 module.exports = router;
