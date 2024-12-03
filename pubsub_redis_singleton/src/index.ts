ee9e69251c2af9f98b97fed61a923eaa179839d6
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-12-03 22:22:13 +0530

    Added subscription in pubsub

--- pubsub_redis_singleton/src/index.ts
+++ pubsub_redis_singleton/src/index.ts
@@ -1,5 +1,18 @@
-import { pubSubManager } from "./pubSubManager"
+import { pubSubManager } from "./pubSubManager";
 
-setInterval(()=>{
-    pubSubManager.getInstance().userSubscribe(Math.random().toString());
-},5000)
+const pubSub = pubSubManager.getInstance();
+const redispublisher = pubSub.getPublisherClient();
+
+// Users subscribe to different stocks
+pubSub.userSubscribe("user3", "AAPL");
+pubSub.userSubscribe("user4", "AAPL");
+pubSub.userSubscribe("user1", "MSFT");
+
+// Simulate messages being published
+redispublisher.publish("AAPL", "Apple stock price is $155");
+redispublisher.publish("MSFT", "Microsoft stock price is $310");
+
+// Delay for message handling
+setTimeout(() => {
+    pubSub.disconnect();
+}, 1000);
--- pubsub_redis_singleton/src/pubSubManager.ts
+++ pubsub_redis_singleton/src/pubSubManager.ts
@@ -2,27 +2,77 @@ import { createClient, RedisClientType } from "redis";
 
 export class pubSubManager {
     private static instance: pubSubManager;
-    private redisClient: RedisClientType;
+    public redisClient: RedisClientType;
+    private publisherClient: RedisClientType;
     private subscriptions: Map<string, string[]>;
+    // static redisClient: RedisClientType;
 
     private constructor() {
         this.redisClient = createClient();
+        this.publisherClient = createClient();
+        this.publisherClient.connect();
         this.redisClient.connect();
         this.subscriptions = new Map();
     }
 
-    public static getInstance() {
+    public static getInstance(): pubSubManager {
         if (!pubSubManager.instance) {
             pubSubManager.instance = new pubSubManager();
         }
         return pubSubManager.instance;
     }
 
-    public userSubscribe(){}
+    public getPublisherClient(): RedisClientType{
+        return this.publisherClient;
+    }
 
-    public userUnsubscribe(){}
 
-    public handleMessage(){}
+    public userSubscribe(userId: string, stock: string) {
+        if (!this.subscriptions.has(stock)) {
+            console.log(`2 :- ${this.subscriptions.has(stock)}`);
+            console.log(`subscriptions: ${JSON.stringify(Object.fromEntries(this.subscriptions))}, stock: ${stock}`);
+            this.subscriptions.set(stock, []);
+            console.log(`1 :- ${JSON.stringify(this.subscriptions.get(stock))}`);
+        }
 
-    public async disconnect(){}
+        this.subscriptions.get(stock)?.push(userId);
+        console.log(`pushing ${userId} into ${this.subscriptions.get(stock)}`);
+
+        if (this.subscriptions.get(stock)?.length === 1) {
+            console.log(`length:- ${this.subscriptions.get(stock)?.length}, ${this.subscriptions.get(stock)}`);
+
+            this.redisClient.subscribe(stock, (message) => {
+                console.log(`stock:- ${stock} message:- ${message}`);
+                this.handleMessage(stock, message);
+            })
+        }
+
+        // this.redisClient.on('subscribe', (channel:string, count:number) => {
+        //     console.log(`Subscribed to ${channel}. Total subscriptions: ${count}`);
+        // });
+        
+
+        console.log(`Subscribed to Redis channel: ${stock}`);
+
+    }
+
+    public userUnsubscribe(userId: string, stock: string) {
+        this.subscriptions.set(stock, this.subscriptions.get(stock)?.filter(id => id != userId) || []);
+
+        if (this.subscriptions.get(stock)?.length === 0) {
+            this.redisClient.unsubscribe(stock);
+            console.log(`UnSubscribed to Redis channel: ${stock}`);
+        }
+    }
+
+    public handleMessage(stock: string, message: string) {
+        console.log(`Message received on channel ${stock}: ${message}`);
+        this.subscriptions.get(stock)?.forEach((sub) => {
+            console.log(`Sending message to user: ${sub}`);
+        })
+    }
+
+    public async disconnect() {
+        await this.redisClient.quit();
+    }
 }
